fceb59c7d3ced5dd2fc41adf33ed944b4afab019
ShehzorMemon <Shezymemon0180@gmail.com>
2025-03-11 06:47:10 +0500

    made routes, navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import styles from "./App.module.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <BrowserRouter>
+        <div className={styles.layout}>
+          <Navbar />
+          <Routes>
+            <Route 
+              path="/" 
+              exact 
+              element={<div className={styles.main}>CoinBounce</div>} 
+            />
+            <Route 
+              path="/" 
+              exact 
+              element={<div className={styles.main}><Home /></div>} 
+            />
+            <Route 
+              path="/crypto" 
+              exact 
+              element={<div className={styles.main}>CryptoPage</div>} 
+            />
+            <Route 
+              path="/blogs" 
+              exact 
+              element={<div className={styles.main}>BlogsPage</div>} 
+            />
+            <Route 
+              path="/submit" 
+              exact 
+              element={<div className={styles.main}>SubmitBlog </div>} 
+            />
+            <Route 
+              path="/sign-up" 
+              exact 
+              element={<div className={styles.main}>SignIn</div>} 
+            />
+            <Route 
+              path="/log-in" 
+              exact 
+              element={<div className={styles.main}>Login</div>} 
+            />
+
+
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <p className={styles.footer}>
+      &copy; CoinBouncer 2025
+    </p>
+  )
+}
+
+export default Footer
