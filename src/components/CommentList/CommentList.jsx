9b338af03a945515745225fab065b27746c6c0ea
ShehzorMemon <Shezymemon0180@gmail.com>
2025-03-22 22:22:47 +0500

    Completed styling of CommentList component

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -5,13 +5,15 @@ import styles from "./CommentList.module.css"
 const CommentList = ({comments}) => {
   return (
     <div className={styles.CommentListWrapper}>
-      {comments.length === 0 ? 
-      (
-        <div className={styles.noComments}>No comments posted</div>
-      ) : 
-      comments.map(comment => (
-        <Comment key={comment._id} comment={comment} />
-      ))}
+      <div className={styles.commentList}>
+        {comments.length === 0 ? 
+        (
+          <div className={styles.noComments}>No comments posted</div>
+        ) : 
+        comments.map(comment => (
+          <Comment key={comment._id} comment={comment} />
+        ))}
+      </div>
     </div>
   )
 }
